24d2587eabb763399bb23e2743980363b58ce492
StrongLeg <ramonprice96@gmail.com>
2025-07-31 10:40:13 +0300

    Отредактировал дешборд

--- views/dashboard.py
+++ views/dashboard.py
@@ -5,7 +5,7 @@ class DashboardPage(QWidget):
     def __init__(self):
         super().__init__()
         layout = QVBoxLayout()
-        label = QLabel("<h2>Главный экран</h2><p>Краткая статистика, последние измерения, уведомления.</p>")
+        label = QLabel("<h2>Главный экран</h2><p>Краткая статистика, последние измерения, уведомления и т.д..</p>")
         label.setAlignment(Qt.AlignCenter)  # Теперь Qt доступен
         layout.addWidget(label)
         self.setLayout(layout)
