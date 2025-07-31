90b064eb1fb2b63ca89986160f0f4baf5bf7fe4a
alexn-hub <alexn-90@mail.ru>
2025-07-31 12:50:26 +0300

    Первая редакция Ввода

--- views/data/composition.py
+++ views/data/composition.py
@@ -5,7 +5,7 @@ class CompositionPage(QWidget):
     def __init__(self):
         super().__init__()
         layout = QVBoxLayout()
-        label = QLabel("<h2>Ввод химических содержаний</h2><p>Ручной ввод эталонов.</p>")
+        label = QLabel("<h1>Ввод химических содержаний</h1><p>Ручной ввод эталонов.</p>")
         label.setAlignment(Qt.AlignCenter)  # Теперь Qt доступен
         layout.addWidget(label)
         self.setLayout(layout)
